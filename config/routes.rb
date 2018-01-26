6a9a07e2fd87978bd4283f46adb71a75483119d9
Chris Torres <hwct83@gmail.com>
2018-01-26 14:01:08 +0700

    5.1 changed root to index page, created links to all pages, redirect to landing page after new product created

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'simple_pages/contact'
 
   get 'simple_pages/index'
-  root 'simple_pages#landing_page'
+  root 'simple_pages#index'
 
   resources :orders, only: [:index, :show, :create, :destroy]
 
