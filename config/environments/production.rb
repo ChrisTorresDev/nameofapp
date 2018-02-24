a7cc2705ce4828fe0432904cb1388b436690e535
Chris Torres <hwct83@gmail.com>
2018-02-24 17:13:56 +0700

    6.13 added real-time updates with ActionCable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.web_socket_server-url = "wss://geardventure.heroku.com/cable"
+  config.action_cable.allowed_request_origins = ['https://geardventure.herokuapp.com', 'http://geardventure.herokuapp.com']
 end
